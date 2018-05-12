e36afd593c3098a5cf1195adda10238bae4a4c20
joaoasrosa <joaoasrosa@gmail.com>
2018-05-12 15:45:09 +0200

    Initial acceptance test

---
+++ tests/Lambda.Tests.Acceptance/Send_Sms_Feature.Steps.cs
@@ -0,0 +1,27 @@
+﻿using LightBDD.XUnit2;
+
+namespace Lambda.Tests.Acceptance
+{
+    public partial class Send_Sms_Feature : FeatureFixture
+    {
+        private void Invalid_Api_Credentials()
+        {
+            // Start the container with the proper env
+            // Start the lambda container using the API command
+            throw new System.NotImplementedException();
+        }
+
+        private void The_user_tries_to_send_an_sms()
+        {
+            // Post to the API endpoint
+            // Collect the result
+            throw new System.NotImplementedException();
+        }
+
+        private void The_Api_returns_Unauthorized()
+        {
+            //Assert the result
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/Lambda.Tests.Acceptance/Send_Sms_Feature.cs
@@ -0,0 +1,26 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios.Extended;
+using LightBDD.XUnit2;
+
+[assembly: LightBddScope]
+
+namespace Lambda.Tests.Acceptance
+{
+    [FeatureDescription(
+        @"As a user
+I want to send a SMS
+So I can communicate")]
+    public partial class Send_Sms_Feature
+    {
+        [Scenario]
+        [Label("Invalid credentials")]
+        public void Invalid_Credentials()
+        {
+            Runner.RunScenario(
+                given => Invalid_Api_Credentials(),
+                when => The_user_tries_to_send_an_sms(),
+                then => The_Api_returns_Unauthorized()
+            );
+        }
+    }
+}
