d33c8e197225a538aace22c838105c3aa3c6bcf3
joaoasrosa <joaoasrosa@gmail.com>
2018-05-12 10:39:44 +0200

    Add DTO

---
+++ tests/Lambda.Api.Tests/Dtos/Sms.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Lambda.Api.Tests.Dtos
+{
+    public class Sms
+    {
+        private readonly string _body;
+        private readonly string _to;
+        private readonly string _type;
+
+        [JsonConstructor]
+        internal Sms(
+            string to,
+            string body,
+            string type)
+        {
+            _to = to;
+            _body = body;
+            _type = type;
+        }
+    }
+}
