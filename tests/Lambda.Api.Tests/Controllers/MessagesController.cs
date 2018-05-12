80bd5ae132e9c6de0e04a55101386a84f81e4f7d
joaoasrosa <joaoasrosa@gmail.com>
2018-05-12 10:15:06 +0200

    Add Testing API

---
+++ tests/Lambda.Api.Tests/Controllers/MessagesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lambda.Api.Tests.Controllers
+{
+    public class MessagesController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
