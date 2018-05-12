cf26bb0447f963c70d6beb98f111f86c5dd9b394
joaoasrosa <joaoasrosa@gmail.com>
2018-05-12 20:06:05 +0200

    Update unit tests for new data contract

---
+++ tests/Lambda.Tests.Unit/Builders/APIGatewayProxyRequestBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+
+namespace Lambda.Tests.Unit.Builders
+{
+    internal class APIGatewayProxyRequestBuilder
+    {
+        private object _event;
+
+        internal APIGatewayProxyRequestBuilder WithEvent(
+            object @event)
+        {
+            _event = @event;
+            return this;
+        }
+
+        internal APIGatewayProxyRequest Build()
+        {
+            return new APIGatewayProxyRequest
+            {
+                Body = JsonConvert.SerializeObject(_event)
+            };
+        }
+    }
+}
---
+++ tests/Lambda.Tests.Unit/Notification.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Lambda.Tests.Unit
+{
+    internal class Notification
+    {
+        internal Notification(
+            string phoneNumber,
+            string displayName)
+        {
+            PhoneNumber = phoneNumber;
+            DisplayName = displayName;
+        }
+
+        [JsonProperty]
+        internal string PhoneNumber { get; }
+        
+        [JsonProperty]
+        internal string DisplayName { get; }
+    }
+}
