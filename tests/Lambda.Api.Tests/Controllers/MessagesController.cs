ec25d55c74080703d4122d74b4788aa9d82bc643
joaoasrosa <joaoasrosa@gmail.com>
2018-05-12 10:40:55 +0200

    Add DTO to API method

--- tests/Lambda.Api.Tests/Controllers/MessagesController.cs
+++ tests/Lambda.Api.Tests/Controllers/MessagesController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using Lambda.Api.Tests.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lambda.Api.Tests.Controllers
 {
     public class MessagesController : Controller
     {
-        public IActionResult Index()
+        [HttpPost("v1/messages")]
+        public IActionResult Send(
+            [FromBody] Sms sms)
         {
             throw new NotImplementedException();
         }
