d8afc7d064deb94c99cb237686b6ba4abf5b2aa7
joaoasrosa <joaoasrosa@gmail.com>
2018-05-16 12:39:12 +0200

    List the serice on port 8000

--- build/configure_appveyor_agent.ps1
+++ build/configure_appveyor_agent.ps1
@@ -1,5 +1,8 @@
 Import-Module -Name NetNat
 Get-NetNat | Remove-NetNat -Confirm:$false # https://github.com/docker/for-win/issues/598
+
+netstat -ano | findstr "8000" | Select-String -Pattern "\d+$" -AllMatches |  % {$_.Matches } | foreach { tasklist |select-string -Pattern $_.Value } | Get-Unique
+
 python -m pip install --upgrade pip
 pip install requests[security]
 $env:PATH += ";C:\Users\appveyor\AppData\Roaming\Python\Scripts"
