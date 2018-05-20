c573517c091c5084d1548403cf1b79ec3de94cae
joaoasrosa <joaoasrosa@gmail.com>
2018-05-20 21:22:24 +0200

    Remove Nat commands

--- build/configure_appveyor_agent.ps1
+++ build/configure_appveyor_agent.ps1
@@ -1,6 +1,3 @@
-Import-Module -Name NetNat -Verbose
-Get-NetNat | Remove-NetNat -Confirm:$false -Verbose # https://github.com/docker/for-win/issues/598
-
 python -m pip install --upgrade pip
 pip install requests[security]
 $env:PATH += ";C:\Users\appveyor\AppData\Roaming\Python\Scripts"
